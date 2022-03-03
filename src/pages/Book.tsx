1dd647a1df3633f6266228136654a1d8e27a4bba
Reskyppo <reskybeen@gmail.com>
2022-03-03 22:11:24 +0700

    layouting: add loading

--- src/pages/Book.tsx
+++ src/pages/Book.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { saveLocalStorage } from "../lib/utils";
 import { Helmet } from "react-helmet";
+import Loading from "../components/Loading";
 
 interface IBook {
   id: number;
@@ -20,6 +21,7 @@ const Book = () => {
 
   const [book, setBook] = useState<IBook>();
   const [isFavorite, setIsFavorite] = useState<boolean>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   let { category, id, page } = useParams();
 
@@ -46,6 +48,7 @@ const Book = () => {
           (book: { id: number }) => book.id === parseInt(id || "")
         );
         setBook(book[0]);
+        setIsLoading(false);
         const res = data.filter(
           (item: { id: number }) => item.id === book[0]?.id
         );
@@ -59,43 +62,49 @@ const Book = () => {
         <title>Booku</title>
       </Helmet>
       <div className="bg-gray-50 min-h-screen">
-        <div className="bg-white max-w-screen shadow-xl py-6">
-          <div className="md:flex justify-center md:w-2/3 lg:w-1/3 md:mx-auto ">
-            <img
-              className="mx-auto w-1/3"
-              src={book?.cover_url}
-              alt={book?.title}
-            />
-            <div className="my-4 ml-4">
-              <h1 className="text-3xl font-bold">{book?.title}</h1>
-              <div className="flex my-2">
-                {book?.authors.map((a: string) => (
-                  <p className="font-semibold mr-2">{a}</p>
-                ))}
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <>
+            <div className="bg-white max-w-screen shadow-xl py-6">
+              <div className="md:flex justify-center md:w-2/3 lg:w-1/3 md:mx-auto ">
+                <img
+                  className="mx-auto w-1/3"
+                  src={book?.cover_url}
+                  alt={book?.title}
+                />
+                <div className="my-4 ml-4">
+                  <h1 className="text-3xl font-bold">{book?.title}</h1>
+                  <div className="flex my-2">
+                    {book?.authors.map((a: string) => (
+                      <p className="font-semibold mr-2">{a}</p>
+                    ))}
+                  </div>
+                  <p className="text-sm">{book?.description}</p>
+                  {!isFavorite && (
+                    <button
+                      className="bg-rose-400 py-2 px-4 mt-2 rounded-md text-white"
+                      onClick={() => setFavorite()}
+                    >
+                      Save as Favorite
+                    </button>
+                  )}
+                </div>
               </div>
-              <p className="text-sm">{book?.description}</p>
-              {!isFavorite && (
-                <button
-                  className="bg-rose-400 py-2 px-4 mt-2 rounded-md text-white"
-                  onClick={() => setFavorite()}
-                >
-                  Save as Favorite
-                </button>
-              )}
             </div>
-          </div>
-        </div>
-        <div className="container bg-white rounded-xl shadow-md p-4 mx-0 md:mx-auto mt-12">
-          <h3 className="text-2xl font-bold"> List Chapters</h3>
-          {book?.sections?.map((section, idx) => (
-            <div className="my-4" key={idx}>
-              <p className="font-semibold text-lg w-fit border-b-2 border-indigo-500">
-                {section?.title}
-              </p>
-              <p className="mt-2">{section?.content}</p>
+            <div className="container bg-white rounded-xl shadow-md p-4 mx-0 md:mx-auto mt-12">
+              <h3 className="text-2xl font-bold"> List Chapters</h3>
+              {book?.sections?.map((section, idx) => (
+                <div className="my-4" key={idx}>
+                  <p className="font-semibold text-lg w-fit border-b-2 border-indigo-500">
+                    {section?.title}
+                  </p>
+                  <p className="mt-2">{section?.content}</p>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
+          </>
+        )}
       </div>
     </>
   );
